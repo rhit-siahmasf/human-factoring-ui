d51419d6e1575b6d5f04c36fa6bcc7d26c6b46de
rhit-siahmasf <60992895+rhit-siahmasf@users.noreply.github.com>
2021-07-25 19:04:50 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		NavigationScreen ss = new NavigationScreen();
+		ss.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ss.setLocationRelativeTo(null);
+		ss.setResizable(false);
+		ss.setVisible(true);
+		
+	}
+}
